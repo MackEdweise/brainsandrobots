98f65c52f9f666ef1e8b1c7264bd4480ca839f06
MackEdweise <marcusedwards@Hotmail.ca>
2015-04-23 17:53:17 -0400

    Create left_remote.py
    Simple program for remote trigger of left turn.

---
+++ left_remote.py
@@ -0,0 +1,11 @@
+    import time
+    import RPi.GPIO as GPIO
+    
+    GPIO.setmode(BCM)
+    GPIO.setup(25, GPIO.OUT)
+    
+    GPIO.output(25,1)
+    time.sleep(3)
+    GPIO.output(25,0)
+    
+    GPIO.cleanup()
