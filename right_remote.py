a4ac778dbfcc3d51b97abc3abcd9fb94693d59e2
MackEdweise <marcusedwards@Hotmail.ca>
2015-04-23 18:19:04 -0400

    Update right_remote.py
    Fix indent.

--- right_remote.py
+++ right_remote.py
@@ -1,11 +1,11 @@
-    import time
-    import RPi.GPIO as GPIO
+import time
+import RPi.GPIO as GPIO
     
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(27, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(27, GPIO.OUT)
     
-    GPIO.output(27,1)
-    time.sleep(3)
-    GPIO.output(27,0)
+GPIO.output(27,1)
+time.sleep(3)
+GPIO.output(27,0)
     
-    GPIO.cleanup()
+GPIO.cleanup()
