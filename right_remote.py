000e2daf0c9886579ba7fe36c08509e72d47460c
MackEdweise <marcusedwards@Hotmail.ca>
2015-04-23 17:51:38 -0400

    Create right_remote.py
    Simple program for remote trigger of right turn.

---
+++ right_remote.py
@@ -0,0 +1,11 @@
+    import time
+    import RPi.GPIO as GPIO
+    
+    GPIO.setmode(BCM)
+    GPIO.setup(27, GPIO.OUT)
+    
+    GPIO.output(27,1)
+    time.sleep(3)
+    GPIO.output(27,0)
+    
+    GPIO.cleanup()
