5237611b3c8f61bd08c18b93c6104ba4a9164687
MackEdweise <marcusedwards@Hotmail.ca>
2015-04-23 18:19:33 -0400

    Update forward_remote.py
    Fix indent.

--- forward_remote.py
+++ forward_remote.py
@@ -1,16 +1,16 @@
-    import RPi.GPIO as GPIO
-    import time
+import RPi.GPIO as GPIO
+import time
     
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(25, GPIO.OUT)
-    GPIO.setup(27, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
     
-    GPIO.output(25,1)
-    time.sleep(3)
-    GPIO.output(25,0)
+GPIO.output(25,1)
+time.sleep(3)
+GPIO.output(25,0)
     
-    GPIO.output(27,1)
-    time.sleep(6)
-    GPIO.output(27,0)
+GPIO.output(27,1)
+time.sleep(6)
+GPIO.output(27,0)
     
-    GPIO.cleanup()
+GPIO.cleanup()
