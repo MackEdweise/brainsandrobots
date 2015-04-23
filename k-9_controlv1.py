140cf34a7010f083960e9152f8498f28a2d71e95
MackEdweise <marcusedwards@Hotmail.ca>
2015-04-23 17:36:08 -0400

    Update k-9_controlv1.py
    Change gpio mode to BCM.

--- k-9_controlv1.py
+++ k-9_controlv1.py
@@ -3,9 +3,10 @@ import os
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(22, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
-GPIO.setmode(GPIO.BOARD)
+
 
 class App:
   def __init__(self, master):
